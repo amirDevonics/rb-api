f7b64d80e6fd166c15903f2f7eb01081bc48d3c8
amirmagictv <amirk@magictvbox.com>
2024-03-21 13:43:27 -0700

    fix line 786|mispelled state

---
+++ main.py
@@ -0,0 +1,9 @@
+from python.rainbow.rainbow import cobot
+# This IP address can be found in the control box.
+cobot.ToCB('10.0.0.8') 
+
+# Initialize (activate) the cobot.
+cobot.CobotInit()
+print(cobot.GetCurrentCobotStatus())
+# In real mode, you need to be careful as the cobot is actually moving.
+cobot.SetProgramMode(cobot.PG_MODE.REAL)
